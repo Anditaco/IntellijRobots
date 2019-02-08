2cb7322a4177453a2a722bfaa8d1b9fc9bea9df8
Anditaco <trioquark3@gmail.com>
2019-02-08 09:54:20 -0500

    Continued work on projections of robots paths

--- IntellijRobot.java
+++ IntellijRobot.java
@@ -42,6 +42,13 @@ public class IntellijRobot extends AdvancedRobot {
         g.setColor(Color.GREEN);
         g.drawString(Integer.toString(enemies.getEnemyList().size()), 50, 50);
         for(Enemy e : enemies.getEnemyList()) {
+            g.setColor(new Color(255,0,0,128));
+            double[][] projections = e.getProjectionsAtTime(8);
+            for(int i = 0; i < projections.length; i++){
+                g.fillRect((int)projections[i][0] - 5, (int)projections[i][1] - 5, 10, 10);
+                //g.drawLine((int)projections[i][0], (int)projections[i][1],(int)projections[(i+1)%projections.length][0],(int)projections[(i+1)%projections.length][1]);
+            }
+            g.setColor(new Color(0,0,255,128));
             g.drawLine((int)e.getX(), (int)e.getY(), (int) getX(), (int) getY());
             g.fillRect((int)e.getX() - 20, (int)e.getY() - 20, 40, 40);
         }
--- ProbabilityMap.java
+++ ProbabilityMap.java
@@ -18,10 +18,42 @@ class ProbabilityMap {
 
     void updateProbabilities(ScannedRobotEvent newData, Enemy oldData){
         long dT = newData.getTime() - oldData.getTime();
+        if(dT > 8) return;
         intellijRobot.out.println(dT + " ticks happened since " + oldData.getName() + " was last updated");
 
         //Calculate their estimated data if everything stayed the same
         double turnRate = Rules.getTurnRateRadians(oldData.getSpeed());
+        double slowRate = Rules.DECELERATION;
+        double speedRate = Rules.ACCELERATION;
+
+
+        double sNTFSD = 0;
+        for(int i = 0; i < dT; i++){
+            if(oldData.getSpeed() - slowRate*i >= 0){
+                sNTFSD += oldData.getSpeed() - slowRate*i;
+            }
+            else{
+                sNTFSD -= i-oldData.getSpeed()/slowRate * speedRate;
+            }
+        }
+        double avgSpeedNTFSD = sNTFSD/dT;
+        double xNoTurnFullSlow = avgSpeedNTFSD * Math.cos(Math.PI/2 + oldData.getHeadingR()) + oldData.getX();
+        double yNoTurnFullSlow = oldData.getSpeed() * Math.sin(Math.PI/2 + oldData.getHeadingR()) + oldData.getY();
+
+        double xNoTurnNoSlow = oldData.getSpeed() * Math.cos(Math.PI/2 + oldData.getHeadingR()) + oldData.getX();
+        double yNoTurnNoSlow = oldData.getSpeed() * Math.sin(Math.PI/2 + oldData.getHeadingR()) + oldData.getY();
+
+        double xTurningRightNoSlow = (oldData.getSpeed()/turnRate) * Math.cos(Math.PI - oldData.getHeadingR() + turnRate*dT) + oldData.getX();
+        double yTurningRightNoSlow = (oldData.getSpeed()/turnRate) * Math.sin(Math.PI - oldData.getHeadingR() + turnRate*dT) + oldData.getY();
+
+        double xTurningLeftNoSlow = (oldData.getSpeed()/(turnRate*-1)) * Math.cos(Math.PI - oldData.getHeadingR() + turnRate*dT*-1) + oldData.getX();
+        double yTurningLeftNoSlow = (oldData.getSpeed()/(turnRate*-1)) * Math.sin(Math.PI - oldData.getHeadingR() + turnRate*dT*-1) + oldData.getY();
+
+
+        oldData.setProjections(xNoTurnFullSlow, yNoTurnFullSlow, xNoTurnNoSlow, yNoTurnNoSlow, xTurningRightNoSlow, yTurningRightNoSlow, xTurningLeftNoSlow, yTurningLeftNoSlow);
+
+        //TODO
+        //MAKE THE PROJECTIONS DRAW oN THE SCREEN
 
 
         //check against current data
